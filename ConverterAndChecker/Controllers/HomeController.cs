dcf3513e7556a113991b486995079b1ec8d8f85c
MadiyarTastanbekov <@MadiyarTastanbekov@gmail.com>
2024-02-23 00:13:19 +0600

    [fix] Excel ExcelRow

--- ConverterAndChecker/Controllers/HomeController.cs
+++ ConverterAndChecker/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using Microsoft.AspNetCore.Mvc;
 using NPOI.HSSF.UserModel;
+using NPOI.SS.Formula.Functions;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using OfficeOpenXml;
@@ -18,10 +19,15 @@ public class HomeController : Controller
 
     private DateTime StartDate { get; set; }
     private DateTime EndDate { get; set; }
+    private readonly IConfiguration _configuration;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly int cellCount;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
     {
+        _configuration = configuration;
         _logger = logger;
+        cellCount = 53;
     }
 
     public IActionResult Index()
@@ -77,13 +83,99 @@ public class HomeController : Controller
             }
         }
 
+        var excelRow = ExtractDataFromExcel(model.XlsxFile);
+
+        Dictionary<string, ExcelRows> ExcelKeyValuePairs = new();
+        foreach (var val in excelRow)
+        {
+            string key = val.Fio;
+            if (ExcelKeyValuePairs.ContainsKey(key))
+            {
+                var temp = ExcelKeyValuePairs[key];
+                temp.ExcelRow.Add(val);
+                temp.Amount += val.Amount;
+            }
+            else
+            {
+                ExcelRows ers = new();
+                ers.ExcelRow = new();
+                ers.ExcelRow.Add(val);
+                ers.Amount = val.Amount;
+                ExcelKeyValuePairs.Add(key, ers);
+            }
+        }
 
 
-        ExtractDataFromExcel(model.XlsxFile);
+        Dictionary<string, (decimal, string, string)> diffPdfExclSum = new Dictionary<string, (decimal, string, string)>();
+        string comment = "";
+        string color = "green";
+        foreach (var val in ExcelKeyValuePairs)
+        {
+            if (keyValuePairs.ContainsKey(val.Key))
+            {
+                var temp = keyValuePairs[val.Key];
 
-        return RedirectToAction("Index"); // Redirect to another action after uploading
+                if (!diffPdfExclSum.ContainsKey(val.Key))
+                {
+                    decimal diffSum = val.Value.Amount - temp.Amount;
+
+                    if (diffSum > 0)
+                    {
+                        comment = "Excel Amount more than PDF.  Profit = " + diffSum + "; ExcelSum = " + val.Value.Amount + " PdfSum = " + temp.Amount;
+                        color = "yellow";
+                    }
+                    else if (diffSum < 0)
+                    {
+                        comment = "Pdf Amount more than Excel.  Deficit = " + diffSum + "; ExcelSum = " + val.Value.Amount + " PdfSum = " + temp.Amount;
+                        color = "red";
+                    }
+                    diffPdfExclSum.Add(val.Key, (diffSum, comment, color));
+                }
+                keyValuePairs.Remove(val.Key);
+            }
+            else
+            {
+                diffPdfExclSum.Add(val.Key, (val.Value.Amount, "Not Found Client  from PDF", "rrrr"));
+            }
+        }
+
+        foreach (var val in keyValuePairs)
+        {
+            diffPdfExclSum.Add(val.Key, (val.Value.Amount, "Not Found Client  from Excel", "ttt"));
+        }
+
+
+        return View(diffPdfExclSum);
+        //return RedirectToAction("Index"); // Redirect to another action after uploading
     }
+    public void saveExcel(IFormFile XlsxFile)
+    {
 
+        using var stream = XlsxFile.OpenReadStream();
+        IWorkbook workbook;
+        if (XlsxFile.FileName.EndsWith(".xlsx"))
+            workbook = new XSSFWorkbook(stream);
+        else if (XlsxFile.FileName.EndsWith(".xls"))
+            workbook = new HSSFWorkbook(stream);
+        else
+            throw new Exception("Unsupported file format.");
+
+        ISheet sheet = workbook.GetSheetAt(0);
+
+        FileInfo inputFile = new FileInfo(XlsxFile.FileName);
+        //using (ExcelPackage package = new ExcelPackage(inputFile))
+        //{
+        //    // Get the first worksheet in the Excel file
+        //    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+
+        //    // Modify data as needed
+        //    worksheet.Cells["A1"].Value = "Modified Value";
+
+        //    // Save changes to a new Excel file
+        //    FileInfo outputFile = new FileInfo(outputFilePath);
+        //    package.SaveAs(outputFile);
+        //}
+    }
 
     public List<TableRow> ParseTableFromPage(string pageText)
     {
@@ -91,13 +183,13 @@ public class HomeController : Controller
 
         // Define regular expression patterns to match each row of the table
         string pattern = @"(\d+)\s+(\p{L}+\s+\p{L}+\s+\p{L}+)\s+(\d{12})\s+(\S+)\s+([\d,]+\.\d{2})";
-        Regex regex = new Regex(pattern, RegexOptions.Multiline);
-        Regex periodPattern = new Regex(@"Период\s*:\s*([\d.]+)\s*-\s*([\d.]+)");
+        Regex regex = new(pattern, RegexOptions.Multiline);
+        Regex periodPattern = new(@"Период\s*:\s*([\d.]+)\s*-\s*([\d.]+)");
 
 
         // Match rows using the regular expression pattern
         MatchCollection matches = regex.Matches(pageText);
-        Match periodMatch = periodPattern.Match(pageText);
+        System.Text.RegularExpressions.Match periodMatch = periodPattern.Match(pageText);
 
         if (periodMatch.Captures.Count != 0)
         {
@@ -106,7 +198,7 @@ public class HomeController : Controller
         }
 
         // Extract data from each match and create TableRow objects
-        foreach (Match match in matches.Cast<Match>())
+        foreach (System.Text.RegularExpressions.Match match in matches.Cast<System.Text.RegularExpressions.Match>())
         {
             TableRow row = new();
             row.Number = match.Groups[1].Value;
@@ -125,6 +217,12 @@ public class HomeController : Controller
         public List<TableRow> TableRow { get; set; }
     }
 
+    public class ExcelRows
+    {
+        public Decimal Amount { get; set; }
+        public List<ExcelRow> ExcelRow { get; set; }
+    }
+
     public class TableRow
     {
         public string Number { get; set; }
@@ -134,11 +232,17 @@ public class HomeController : Controller
         public decimal Amount { get; set; }
     }
 
+    public class ExcelRow
+    {
+        public string Number { get; set; }
+        public string Fio { get; set; }
+        public decimal Amount { get; set; }
+    }
+
     public List<TableRow> ExtractTextFromPdf(IFormFile pdfFile)
     {
-        StringBuilder tableData = new StringBuilder();
         List<TableRow> pdfdatas = new();
-        using (PdfReader reader = new PdfReader(pdfFile.OpenReadStream()))
+        using (PdfReader reader = new(pdfFile.OpenReadStream()))
         {
             for (int pageNumber = 1; pageNumber <= reader.NumberOfPages; pageNumber++)
             {
@@ -152,31 +256,91 @@ public class HomeController : Controller
 
     //public void Ex
 
-    public void ExtractDataFromExcel(IFormFile excelFile)
+    public List<ExcelRow> ExtractDataFromExcel(IFormFile excelFile)
     {
         using var stream = excelFile.OpenReadStream();
         IWorkbook workbook;
         if (excelFile.FileName.EndsWith(".xlsx"))
             workbook = new XSSFWorkbook(stream);
-        else if (excelFile.FileName.EndsWith(".xls"))   
+        else if (excelFile.FileName.EndsWith(".xls"))
             workbook = new HSSFWorkbook(stream);
         else
             throw new Exception("Unsupported file format.");
 
-        ISheet sheet = workbook.GetSheetAt(0); // Assuming the data is in the first sheet
+        ISheet sheet = workbook.GetSheetAt(0);
 
-        // Loop through rows and columns to extract data
+        int transferredToTheBankCell = -1;
+        int queueRow = -1;
+        string queueIcon = "№ п/п";
+        int fioRow = -1;
+        bool hasHeader = false;
+        List<ExcelRow> rows = new List<ExcelRow>();
+
+
         for (int row = 0; row <= sheet.LastRowNum; row++)
         {
             IRow currentRow = sheet.GetRow(row);
             if (currentRow != null)
             {
-                for (int col = 0; col < currentRow.Cells.Count; col++)
+                if (!hasHeader)
                 {
-                    Console.Write(currentRow.GetCell(col)?.ToString() + "\t");
+                    for (int col = 0; col < currentRow.Cells.Count; col++)
+                    {
+                        if (transferredToTheBankCell == -1 || queueRow == -1 || fioRow == -1)
+                        {
+                            string val = currentRow.GetCell(col)?.ToString() ?? "";
+                            if (val.Equals("Перечислено в банк"))
+                            {
+                                transferredToTheBankCell = col;
+                            }
+                            else if (val.Equals(queueIcon))
+                            {
+                                queueRow = col;
+                            }
+                            else if (val.Equals("Физ. лицо"))
+                            {
+                                fioRow = col;
+                            }
+                        }
+                        else
+                        {
+                            hasHeader = true;
+                            break;
+                        }
+                        Console.Write(currentRow.GetCell(col)?.ToString() + "\t");
+                    }
                 }
+                else
+                {
+                    ExcelRow excelRow = new ExcelRow();
+                    string q = currentRow.GetCell(queueRow)?.ToString() ?? "";
+                    int qq = -1;
+
+                    try
+                    {
+
+                        qq = Convert.ToInt32(q);
+                        if (qq != -1)
+                        {
+                            Console.WriteLine(qq);
+
+                            excelRow.Number = q;
+                            excelRow.Fio = currentRow.GetCell(fioRow)?.ToString() ?? "";
+                            string sum = currentRow.GetCell(transferredToTheBankCell)?.ToString() ?? "0";
+                            excelRow.Amount = Convert.ToDecimal(sum);
+                            rows.Add(excelRow);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+
+                }
+
                 Console.WriteLine();
             }
         }
+
+        return rows;
     }
 }
